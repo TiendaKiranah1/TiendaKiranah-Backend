09dd0e3411c71701f4991f669835efa513caa330
Okeke Chigozie <81691598+bertinchigozie@users.noreply.github.com>
2023-04-18 23:18:04 +0100

    feat:added access control origin

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+
+  next();
+});
+
 /** All routes */
 app.use("/profile", profileRoute);
 app.use("/api/v1/users", facebookRoute);
